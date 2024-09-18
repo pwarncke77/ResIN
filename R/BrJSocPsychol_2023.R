c82072e39ed104898461ae9c332fab0bea363dd5
Philip Warncke <pwarncke@live.unc.edu>
2024-09-18 14:02:41 +0200

    Initial commit

---
+++ R/BrJSocPsychol_2023.R
@@ -0,0 +1,19 @@
+#' Source data for Lüders, A., Carpentras, D. and Quayle, M., 2024. Attitude networks as intergroup realities: Using network‐modelling to research attitude‐identity relationships in polarized political contexts. British Journal of Social Psychology, 63(1), pp.37-51.
+#'
+#' The sample of N = 402 paid participants through the crowd working platform Prolific Academic. The core of the survey consists of A set of eight political attitude items abortion, immigration, gun control, and gay marriage. Each item followed a 5-point scale ranging from strong disagreement to strong agreement. The survey also includes items on partisanship, affective polarization, and a short vignette experiment.
+#'
+#' @docType data
+#'
+#' @usage data(BrJSocPsychol_2023)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @references This dataset was made available by Lüders et.al. (2024)
+#' @keywords datasets
+#' @examples
+#'
+#' data(BrJSocPsychol_2023)
+#' head(BrJSocPsychol_2023)
+#'
+#'
+"BrJSocPsychol_2023"
